43bf9ea037c68cdcb8da23de7f2d965b903a6f15
aLexa163-JV <alekcei-y@ya.ru>
2024-12-08 20:41:27 +0400

    Initial commit

---
+++ src/test/java/cart/cartWH/CartWhApplicationTests.java
@@ -0,0 +1,13 @@
+package cart.cartWH;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartWhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
